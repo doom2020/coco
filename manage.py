cca89c5a4b408c1801825e75504fd5f752eb32ac
yuanzhijian <408575225@qq.com>
2021-06-19 11:28:13 +0800

    更新服务端口、ip

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 """
 程序入口文件
 """
-from flaskapp.settings import DEBUG_MODE
+from flaskapp.settings import DEBUG_MODE, SERVER_IP, SERVER_PORT
 from flaskapp import create_app
 
 
 if __name__ == "__main__":
     app = create_app(DEBUG_MODE)
-    app.run(host='0.0.0.0', port=5000, debug=DEBUG_MODE)
+    app.run(host=SERVER_IP, port=SERVER_PORT, debug=DEBUG_MODE)
    
